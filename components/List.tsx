05335a6f1a785bc04544f3d0ff3687457f3f5208
caiobraconsult <caio@braconsultoria.com.br>
2026-07-03 15:48:11 -0300

    AssignmentIndIcon

--- components/List.tsx
+++ components/List.tsx
@@ -4,15 +4,16 @@ import { App } from "@/Context/AppsContext";
 import { useContext } from "react";
 import { AppsContext } from "@/Context/AppsContext";
 
+import PeopleIcon from '@mui/icons-material/People';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import ChromeReaderModeIcon from "@mui/icons-material/ChromeReaderMode"
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+
+import Image from "next/image";
+
 import {
-  CircleUser,
-  ListCheck,
   IdCardLanyard,
-  FileCog,
-  FilePen,
-  Landmark,
-  HandCoins,
-  BanknoteArrowUp,
   ChartNoAxesCombined,
 } from "lucide-react";
 
@@ -22,14 +23,14 @@ const apps: App[] = [
   {
     id: 1,
     name: "Social",
-    icon: <CircleUser size={24} />,
+    icon: <PeopleIcon />,
     url: "https://www.onyxerp.com.br/Social/",
     description: "Gerenciamento de cadastros de pessoas do RPPS",
   },
   {
     id: 2,
     name: "Processos",
-    icon: <FilePen size={24} />,
+    icon: <Image alt="Processos" src="https://storage3.onyxerp.com.br/logo_processos_bra_branco.png" width={12} height={12} className="" />,
     url: "https://www.onyxerp.com.br/Processos/",
     description:
       "Gestão de processos, simulações de benefícios e parametrização de leis",
@@ -37,7 +38,7 @@ const apps: App[] = [
   {
     id: 3,
     name: "GIR",
-    icon: <BanknoteArrowUp size={24} />,
+    icon: <Image alt="GIR" src="https://onyxerp.s3.us-east-2.amazonaws.com/logo-gir.png" height={24} width={24} className="material-icons" />,
     url: "https://www.onyxerp.com.br/Gir/",
     description:
       "Gestão de Guias de Recolhimento, tratamento e leitura de dados com IA",
@@ -45,7 +46,7 @@ const apps: App[] = [
   {
     id: 4,
     name: "Recad",
-    icon: <ListCheck size={24} />,
+    icon: <DoneAllIcon />,
     url: "https://www.onyxerp.com.br/Recad/",
     description:
       "Tratamento e higienização de Cadastros do Censo Previdenciário",
@@ -53,7 +54,7 @@ const apps: App[] = [
   {
     id: 5,
     name: "Fopag",
-    icon: <HandCoins size={24} />,
+    icon: <Image src="https://onyxerp.s3.us-east-2.amazonaws.com/logo-fopag.png" alt="fopag" width={24} height={24} />,
     url: "/fopag",
     description:
       "Gestão de folhas de pagamento através do Sistema SIP 7.0 da Fiorilli",
@@ -69,21 +70,21 @@ const apps: App[] = [
   {
     id: 7,
     name: "Flowdocs",
-    icon: <FileCog size={24} />,
+    icon: <ChromeReaderModeIcon />,
     url: "/flowdocs",
     description: "Gestão de documentos e fluxos para prefeitura",
   },
   {
     id: 8,
     name: "Conselho",
-    icon: <Landmark size={24} />,
+    icon: <AccountBalanceIcon />,
     url: "https://www.onyxerp.com.br/Conselho/",
     description: "Gestão de conselhos e órgãos",
   },
   {
     id: 9,
     name: "Cadastro",
-    icon: <IdCardLanyard size={24} />,
+    icon: <AssignmentIndIcon />,
     url: "https://www.onyxerp.com.br/Cadastro/",
     description: "Cadastro de pessoas do RPPS (Uso exclusivo de Guarulhos)",
   },
@@ -102,7 +103,7 @@ const List = () => {
           >
             <div
               onClick={() => redirect(app.url)}
-              className="flex-1 rounded-full border cursor-pointer border-ring p-4 hover:bg-white/10 transition-colors"
+              className="w-16 h-16 flex-shrink-0 rounded-full border border-white/20 bg-transparent flex items-center justify-center cursor-pointer hover:bg-white/10 hover:border-white/40 transition-all duration-300"
               onMouseOver={() => {
                 setApps({
                   name: app.name,
@@ -110,7 +111,9 @@ const List = () => {
                 });
               }}
             >
-              {app.icon}
+              <div className="flex items-center justify-center [&_svg]:size-6 [&_img]:size-6 [&_img]:object-contain">
+                {app.icon}
+              </div>
             </div>
             <span className="text-sm font-medium text-center">{app.name}</span>
           </div>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["storage3.onyxerp.com.br", "onyxerp.s3.us-east-2.amazonaws.com"],
+  },
 };
 
 export default nextConfig;
